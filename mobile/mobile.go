05143a65c9af7f60b841f629aa86bc7823169d3a
billcoding <wangmiao07@sina.com>
2024-09-21 16:46:23 +0800

    feat: fix mobile.go

--- mobile/mobile.go
+++ mobile/mobile.go
@@ -4,33 +4,52 @@ import (
 	"encoding/json"
 
 	"github.com/hiddify/hiddify-core/config"
+	"github.com/sagernet/sing-box/experimental/libbox"
 	"github.com/sagernet/sing-box/option"
 
 	_ "github.com/sagernet/gomobile"
 )
 
-//export Setup
-func Setup() error {
-	// return config.StartGRPCServer(7078)
-	return nil
+var (
+	defaultOptionJson = ""
+	defaultConfigJson = ""
+	service           *libbox.BoxService
+	commandServer     *libbox.CommandServer
+	commandClient     *libbox.CommandClient
+	commandHandler    = &commandHandlerImpl{}
+	sps               = &libbox.SystemProxyStatus{Available: true, Enabled: false}
+	upLink            int
+	downLink          int
+	serviceStarted    bool
+)
+
+//export Start
+func Start() { serviceStarted = true }
+
+//export Stop
+func Stop() { serviceStarted = false }
+
+//export ServiceStarted
+func ServiceStarted() bool { return serviceStarted }
+
+//export SetOptionJson
+func SetOptionJson(optionJson string) (err error) { defaultOptionJson = optionJson; return }
+
+//export SetConfigJson
+func SetConfigJson(configJson string) (err error) { defaultConfigJson = configJson; return }
+
+//export GetAllJson
+func GetAllJson() (string, error) {
+	return buildConfig(defaultOptionJson, defaultConfigJson)
 }
 
-//	func Parse(path string, tempPath string, debug bool) error {
-//		config, err := config.ParseConfig(tempPath, debug)
-//		if err != nil {
-//			return err
-//		}
-//		return os.WriteFile(path, config, 0644)
-//	}
-//
+//export IpLink
+func UpLink() int { return upLink }
 
-//export BuildConfig
-func BuildConfig(optionJson string, configJson string) (string, error) {
-	// os.Chdir(filepath.Dir(path))
-	// fileContent, err := os.ReadFile(path)
-	// if err != nil {
-	// 	return "", err
-	// }
+//export DownLink
+func DownLink() int { return downLink }
+
+func buildConfig(optionJson string, configJson string) (string, error) {
 	var options option.Options
 	err := options.UnmarshalJSON([]byte(configJson))
 	if err != nil {
@@ -42,22 +61,66 @@ func BuildConfig(optionJson string, configJson string) (string, error) {
 		return "", nil
 	}
 	if configOptions.Warp.WireguardConfigStr != "" {
-		err := json.Unmarshal([]byte(configOptions.Warp.WireguardConfigStr), &configOptions.Warp.WireguardConfig)
+		err = json.Unmarshal([]byte(configOptions.Warp.WireguardConfigStr), &configOptions.Warp.WireguardConfig)
 		if err != nil {
 			return "", err
 		}
 	}
-
 	if configOptions.Warp2.WireguardConfigStr != "" {
-		err := json.Unmarshal([]byte(configOptions.Warp2.WireguardConfigStr), &configOptions.Warp2.WireguardConfig)
+		err = json.Unmarshal([]byte(configOptions.Warp2.WireguardConfigStr), &configOptions.Warp2.WireguardConfig)
 		if err != nil {
 			return "", err
 		}
 	}
-
 	return config.BuildConfigJson(*configOptions, options)
 }
 
-// func GenerateWarpConfig(licenseKey string, accountId string, accessToken string) (string, error) {
-// 	return config.GenerateWarpAccount(licenseKey, accountId, accessToken)
-// }
+func StartCommandServer(boxService *libbox.BoxService) (err error) {
+	if boxService == nil {
+		service = boxService
+	}
+	if commandServer == nil {
+		commandServer = libbox.NewCommandServer(commandHandler, 10000)
+		commandServer.SetService(boxService)
+		if err = commandServer.Start(); err != nil {
+			return
+		}
+	}
+	if commandClient == nil {
+		commandClient = libbox.NewCommandClient(commandHandler, &libbox.CommandClientOptions{
+			Command:        libbox.CommandStatus,
+			StatusInterval: 1000000000,
+		})
+		err = commandClient.Connect()
+	}
+	return
+}
+
+func CloseCommandServer() {
+	if commandServer != nil {
+		_ = commandServer.Close()
+		commandServer = nil
+	}
+	if commandClient != nil {
+		_ = commandClient.Disconnect()
+		commandClient = nil
+	}
+}
+
+type commandHandlerImpl struct{}
+
+func (c *commandHandlerImpl) Connected()          {}
+func (c *commandHandlerImpl) Disconnected(string) {}
+func (c *commandHandlerImpl) ClearLog()           {}
+func (c *commandHandlerImpl) WriteLog(string)     {}
+func (c *commandHandlerImpl) WriteStatus(message *libbox.StatusMessage) {
+	upLink = int(message.Uplink)
+	downLink = int(message.Downlink)
+}
+func (c *commandHandlerImpl) WriteGroups(libbox.OutboundGroupIterator)          {}
+func (c *commandHandlerImpl) InitializeClashMode(libbox.StringIterator, string) {}
+func (c *commandHandlerImpl) UpdateClashMode(string)                            {}
+func (c *commandHandlerImpl) PostServiceClose()                                 {}
+func (c *commandHandlerImpl) ServiceReload() (err error)                        { return }
+func (c *commandHandlerImpl) GetSystemProxyStatus() *libbox.SystemProxyStatus   { return sps }
+func (c *commandHandlerImpl) SetSystemProxyEnabled(bool) (err error)            { return }
